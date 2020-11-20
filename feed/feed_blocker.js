3fda01632b91fecd55fe341e9bbd654b4aca9e6f
rikgj <44588337+rikgj@users.noreply.github.com>
2020-11-20 23:39:14 +0100

    checks path length only

--- feed/feed_blocker.js
+++ feed/feed_blocker.js
@@ -4,20 +4,23 @@ This js code will create a feed blocker to site spesification given in domainInf
 let domainInfo = {
   "facebook":{
     "block_parent":"#mount_0_0 > div > div:nth-child(1) > div.rq0escxv.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.g5gj957u.pmt1y7k9.buofh1pr.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div",
-    "eventListener":"click"
   },
   "youtube":{
     "block_parent":"#page-manager > ytd-browse",
-    "eventListener":"yt-navigate-start"
   }
 }
 
 let domain = document.location.href.split(".")[1];
 let info = domainInfo[domain];
-let cur_path = document.location.pathname;
+let path_length = document.location.pathname.length;
 const BLOCK_ID = "feed_blocker";
 let block = false;
 
+// sets block if you navigate directly to main site
+if(path_length == 1){
+  setBlock();
+}
+document.addEventListener("click", () => { checkPath(); });
 
 
 function setBlock(){
@@ -43,27 +46,20 @@ function setBlock(){
 
 function checkPath(){
   setTimeout(()=>{
-    if (cur_path != document.location.pathname) {
-        cur_path = document.location.pathname;
-        if(cur_path.length == 1){
+    let cur_len = document.location.pathname.length
+    if (path_length != cur_len) {
+        path_length = cur_len;
+        if(path_length == 1){
           // there has been navigation to the main site
           if(!block){
             setBlock();
           }else{
             block.hidden = false;
           }
-
-        }else if (cur_path.length > 1 && block){
+        }else if (path_length > 1 && block){
           // nav outisde main site and block is set
           block.hidden = true
         }
     }
   }, 500);
 }
-
-
-// sets block if you navigate directly
-if(cur_path.length == 1){
-  setBlock();
-}
-document.addEventListener("click", () => { checkPath(); });
